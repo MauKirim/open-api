276a5fc634ff330f6f9004853dfad3f8c9b4a6ea
indra <indra.gunanda@dev.nusatek.id>
2023-04-16 01:37:48 +0700

    initialize : dev

--- config/config.php
+++ config/config.php
@@ -4,5 +4,8 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    "token" => env('MAUKIRIM_TOKEN', ''),
+    "dev_url" => "https://dev-api.maukirim.com",
+    "prod_url" => "https://api.maukirim.com",
+    "api_env" => env('MAUKIRIM_ENV', 'prod'),
 ];
--- src/OpenApi.php
+++ src/OpenApi.php
@@ -2,7 +2,76 @@
 
 namespace MauKirim\OpenApi;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+/**
+ * Class OpenApi
+ * @package MauKirim\OpenApi
+ */
 class OpenApi
 {
-    // Build your next great package.
+    public Client $client;
+    public string $connection_name = 'open-api';
+
+    /**
+     * OpenApi constructor.
+     * @param int $timeout
+     */
+    public function __construct(int $timeout)
+    {
+        $this->initClient($timeout);
+        return $this;
+    }
+
+    /**
+     * Initiate Guzzle Client
+     * @param int $timeout
+     * @return void
+     */
+    private function initClient(int $timeout)
+    {
+        $messageFormat = "{host} - {req_header_authorization} {request} {req_body} {error}";
+        $stack = HandlerStack::create();
+        $stack->push(
+            Middleware::log(
+                new Logger($this->connection_name, [new StreamHandler(storage_path('logs/' . $this->connection_name . '.log'))]),
+                new MessageFormatter($messageFormat)
+            )
+        );
+        $baseUrl = config('open-api.dev_url');
+        $token = config('open-api.token');
+        if (config('open-api.prod')) {
+            $baseUrl = config('open-api.prod_url');
+        }
+        $this->client = new Client([
+            'base_uri' => $baseUrl,
+            'timeout' => $timeout,
+            'handler' => $stack,
+            'headers' => [
+                "Authorization" => "Bearer " . $token,
+            ]
+        ]);
+    }
+
+    /**
+     * Initialize OpenApi Static
+     * @param int $timeout
+     * @return OpenApi
+     */
+    public static function init(int $timeout = 10): OpenApi
+    {
+        return new self($timeout);
+    }
+
+    public function ping(): string
+    {
+        return "as";
+    }
+
+
 }
